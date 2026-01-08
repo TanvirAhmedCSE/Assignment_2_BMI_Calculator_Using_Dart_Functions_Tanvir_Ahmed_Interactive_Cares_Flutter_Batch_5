110825a2bfad655f14408e6744a23230bd68eda4
TanvirAhmedCSE <jawadkarim19504@gmail.com>
2026-01-08 20:13:29 +0600

    Initial commit

--- test/assignment_2_bmi_calculator_using_dart_functions_tanvir_ahmed_interactive_cares_flutter_batch_5_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:assignment_2_bmi_calculator_using_dart_functions_tanvir_ahmed_interactive_cares_flutter_batch_5/assignment_2_bmi_calculator_using_dart_functions_tanvir_ahmed_interactive_cares_flutter_batch_5.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
